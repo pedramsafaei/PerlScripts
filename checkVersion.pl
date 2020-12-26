8116824379a5baa477b8253bbcfe6554ac19ea69
pedramsafaei <petersafaei@gmail.com>
2020-12-25 19:52:25 -0800

    fixing formatting issues

--- checkVersion.pl
+++ checkVersion.pl
@@ -1,20 +1,16 @@
-
 #!/usr/bin/perl -w
 
 use strict;
 my $version = `perl -v | grep "version"`;
-if($version eq "") 
-{
-	print $version = 0;
-} 
-elsif($version =~ /((\d)+)\W((\d)+)\W((\d)+)/) 
-{
-	print "$1\.$3\.$5\n";
-	$version = $&;
-} 
-else 
-{
-	$version = 0;
+if ($version eq "") {
+  print $version = 0;
+}
+elsif($version = ~/((\d)+)\W((\d)+)\W((\d)+)/) {
+  print "$1\.$3\.$5\n";
+  $version = $ & ;
+}
+else {
+  $version = 0;
 }
 
 print "Perl version is: $version\n";
